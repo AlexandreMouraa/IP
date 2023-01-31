6a7f6e0cdc0dff9225ef2663b4df6131aeee1476
Alexandre Moura <108504530+AlexandreMouraa@users.noreply.github.com>
2023-01-30 21:46:47 -0300

    Create 2p.c

---
+++ Prova 4/2p.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+ 
+int Achaidentidade(int Tam, int Matrix[Tam][Tam])
+{
+ 
+    int i, j;
+ 
+    for(i = 0; i < Tam; i++)
+        {
+            for(j = 0; j < Tam; j++)
+            {
+                if(i == j)
+                {
+                    if(Matrix[i][j] != 1)
+                    {
+                        return (0);
+                        break;
+                    }
+                }
+                else
+                {
+                    if(Matrix[i][j] != 0)
+                    {
+                        return(0);
+                        break;
+                    }
+                }
+ 
+            }
+        }
+ 
+        return 1;
+ 
+}
+ 
+int main()
+{
+    int N = 0, tamanho = 0, i, j;
+ 
+    scanf("%d", &N);
+ 
+    while(N--)
+    {
+ 
+        scanf("%d", &tamanho);
+ 
+        int matriz[tamanho][tamanho];
+ 
+        for(i = 0; i < tamanho; i++)
+        {
+            for(j = 0; j < tamanho; j++)
+            {
+                scanf("%d", &matriz[i][j]);
+            }
+        }
+ 
+        if(Achaidentidade(tamanho, matriz) == 1)
+            printf("Identidade\n");
+ 
+        else
+            printf("Nao identidade\n");
+ 
+        
+    }
+ 
+    return (0);
+}
